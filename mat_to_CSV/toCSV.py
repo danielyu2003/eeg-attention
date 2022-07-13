919cba6351a91741e00e9a811eeabf22366a33fd
Daniel Yu <88413927+danielyu2003@users.noreply.github.com>
2022-07-13 13:09:58 -0400

    changed csv parser

--- mat_to_CSV/toCSV.py
+++ mat_to_CSV/toCSV.py
@@ -2,6 +2,9 @@ import scipy.io
 import pandas as pd
 import os
 
+
+directory = r"C:\Users\yudan\OneDrive\Desktop\eeg_attention\mat_to_CSV"
+
 def toCSV(subject_num, source):
     '''
     converts the signal data from the .mat files in the as_mat subdirectory to csv files in as_csv
@@ -14,13 +17,14 @@ def toCSV(subject_num, source):
     if not source:
         print("input a source!")
         return
-    directory = os.fsencode('as_mat')
+    
     file_index = 1
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
+    
+    for filename in os.listdir(f"{directory}\\as_mat"):
+        # filename = os.fsdecode(file)
         if filename.endswith(".mat"):
-            print(f"now converting 'as_mat\\{filename}'")
-            mat_data = scipy.io.loadmat(f'as_mat\\{filename}')
+            print(f"now converting {filename}")
+            mat_data = scipy.io.loadmat(f'{directory}\\as_mat\\{filename}')
             
             match source:
             # expand with more cases as needed
@@ -31,7 +35,7 @@ def toCSV(subject_num, source):
             
             df = pd.DataFrame(signal_data)
             # update the subject number below as needed
-            df.to_csv(f'as_csv\\Sub_{subject_num}_Block_{file_index}.csv')
+            df.to_csv(f'{directory}\\as_csv\\Sub_{subject_num}_Block_{file_index}.csv')
             file_index+=1
             continue
         else:
@@ -39,7 +43,7 @@ def toCSV(subject_num, source):
     print("all mat files converted")
     pass
 
-def transposeCSV(csv_table_name, endInd):
+def transposeCSV(csv_table_name, endInd=None):
     '''
     transposes the rows and columns of a given table and saves it
     optionally slice the transposed table past the end index
@@ -48,26 +52,24 @@ def transposeCSV(csv_table_name, endInd):
     @param endInd : int
         positive int indicating the number of columns from the right the dataset should be
     '''
-    df = pd.read_csv(f'as_csv\\{csv_table_name}')
-    df_transposed = df.transpose()
+    df = pd.read_csv(f'{directory}\\as_csv\\{csv_table_name}', header=None)
+    df_transposed = df.transpose().iloc[:,1:]
 
-    if (isInstance(endInd, int)):
+    if (isinstance(endInd, int)):
         df_transposed = crop(df_transposed, endInd)
 
-    df_transposed.to_csv(f'as_csv\\transposed{csv_table_name[:-4]}.csv')
+    df_transposed.to_csv(f'{directory}\\as_csv\\transposed{csv_table_name[:-4]}.csv', header=False)
     pass
 
 def crop(dataframe, endInd):
     return dataframe.iloc[:,:endInd]
 
 def deleteMats():
-    directory = os.fsencode('as_mat')
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
+    for filename in os.listdir(directory):
         if filename.endswith(".mat"):
-            if os.path.exists(f'as_mat\\{filename}'):
+            if os.path.exists(f'{directory}\\as_mat\\{filename}'):
                 print(f"deleting {filename}!")
-                os.remove(f'as_mat\\{filename}')
+                os.remove(f'{directory}\\as_mat\\{filename}')
             else:
                 print("The file does not exist")
                 continue
@@ -75,10 +77,20 @@ def deleteMats():
             continue
     pass
 
+def selectiveCrop(csv_path, indexes):
+    '''
+    @param csv_path : str
+    @param indexes : int[]
+    '''
+    pass
+
 def main():
     print('starting')
-    # toCSV('1', 1)
-    # deleteMats()
+    
+    # toCSV('2', 2)
+    
+    transposeCSV('Sub_2_Block_4.csv', 64)
+    
     print('finished')
     return
 
