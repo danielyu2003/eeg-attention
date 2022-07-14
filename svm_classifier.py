dd802ad20e6c5b03aa745f385bafa852e16c5910
Daniel Yu <88413927+danielyu2003@users.noreply.github.com>
2022-07-14 15:12:03 -0400

    finished implementation
    added classification plotting, metrics

--- svm_classifier.py
+++ svm_classifier.py
@@ -1,6 +1,7 @@
 from sklearn import svm
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import csv
@@ -30,9 +31,11 @@ class EEGClassifier:
         self.classifier = svm.SVC(C=C_val, kernel=kernel_choice, gamma=gamma_choice)
         self.data = data
         self.target = target
+        # splitting the testing and training dataset could be done manually, 
+        # just keep in mind the correct format for numpy array inputs, etc.
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.data, self.target,test_size=test_ratio,random_state=random_seed,shuffle=shuffle_choice)
         
-        self.y_pred = np.zeros(self.x_test.size)
+        self.y_pred = np.zeros(self.y_test.size)
         pass
 
     def fit(self):
@@ -44,12 +47,16 @@ class EEGClassifier:
         pass
 
     def getMetrics(self):
-        '''
-        Note: will not work when there is no proper data and target set
-        '''
         print("Accuracy:",metrics.accuracy_score(self.y_test, self.y_pred))
         print("Precision:",metrics.precision_score(self.y_test, self.y_pred))
         print("Recall:",metrics.recall_score(self.y_test, self.y_pred))
+        pass
+    
+    def plotAcc(self):
+        plt.figure()
+        plt.plot(range(0, len(self.y_test)), self.y_test, 'b', label='truth')
+        plt.plot(range(0,len(self.y_pred)), self.y_pred,'r',label='SVM')
+        plt.show()
         pass
 
 def data(abs_path):
@@ -59,15 +66,48 @@ def data(abs_path):
 def test():
     from sklearn import datasets
     cancer = datasets.load_breast_cancer()
-    # print(type(cancer.data[0]))
     test = EEGClassifier(cancer.data, cancer.target, 0.3, 109)
     test.fit()
     test.predict()
     test.getMetrics()
+    test.plotAcc()
     pass
 
 def main():
+    '''
+    target1, target2, the resting dataset, and the attention dataset should all have the same length/number of samples
+    '''
+    
+    target1 = np.zeros(1000)
+    target2 = np.ones(1000)
+    
+    rest_test = 10 * (np.random.rand(1000, 4) - 0.5)
+    # generates a fake sample of small amplitudes between -5 and 5
+    atten_test = 100 * (np.random.rand(1000, 4) - 0.5)
+    # generated a fake sample of large amplitudes between -50 and 50
+    
+    concat1 = target1.reshape(-1, 1)
+    concat2 = target2.reshape(-1, 1)
+    
+    test1 = np.concatenate((concat1, rest_test), axis=1)
+    test2 = np.concatenate((concat2, atten_test), axis=1)
+    
+    all_samples = np.concatenate((test1, test2))
+    
+    np.random.shuffle(all_samples)
+    
+    seperated = np.hsplit(all_samples, [1])
+    
+    targets = seperated[0].flatten()
+    samples = seperated[1]
+    
+    test = EEGClassifier(samples, targets, 0.3, 109)
+    test.fit()
+    test.predict()
+    test.getMetrics()
+    test.plotAcc()
     pass
 
 if __name__ == '__main__':
-    main()
+    # main()
+    pass
