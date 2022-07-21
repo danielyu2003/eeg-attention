8cf2cc44969cca954e69392a4385cd22d1c3109d
Daniel Yu <88413927+danielyu2003@users.noreply.github.com>
2022-07-21 14:38:58 -0400

    added documentation

--- svm_classifier.py
+++ svm_classifier.py
@@ -5,7 +5,6 @@ from sklearn import svm
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import statistics
 import scipy
 import csv
 import os
@@ -13,58 +12,111 @@ import os
 os.chdir(os.path.dirname(__file__))
 
 class EEGClassifier:
+    '''
+    Binary classification of EEG signal data using the scikit-learn support vector machine implementation
+    This particular script distinguishes between rest and concentration using eye-tracking and visual-haptic feedback
+
+    Attributes:
+    ---
+    kernel_choice : str
+    data : numpy.ndarray.dtype(float64)
+    target : numpy.ndarray.dtype(float64)
+    test_size : float
+    random_state : int
+    shuffle_choice : bool
+
+    Methods:
+    ---
+    fit()
+        Trains the classifier on a portion of the dataset
+    predict()
+        Tests the classifier on a portion of the dataset
+    predictExample(example)
+        Passes a sample to the trained classifier for prediction
+    getMetrics()
+        Displays the metrics of the classifier after self.predict
+    plotAcc()
+        Plots each true label of each sample in the testing set and what the model predicted for each sample
+
+    Functions:
+    ---
+    resample(signal, orig_freq, desired_freq)
+        Resamples a given signal (or an array of signals) from its given frequency to a new one
+
+    '''
     
     def __init__(self, data, target, test_ratio=0.25, random_seed=None, C_val=1.0, kernel_choice='rbf', gamma_choice = 'scale'):
         '''
-        @param kernel_choice : str
+        Parameters:
+        ---
+        kernel_choice : str
             default = 'rbf' (radial basis function)
                 ideal for nonlinearly seperable data
             selects kernel choice for the support vector machine
             see the official scikit-learn website for more details 
             (https://scikit-learn.org/stable/modules/svm.html)
-        @param data : numpy int[]
+        data : numpy.ndarray.dtype(float64)
             an array/matrix of shape (num of samples/y, num of features/x)
-        @param target : numpy int[]
+        target : numpy.ndarray.dtype(int32)
             a 1d array of length (num of samples), where each sample is classified as a 1 or a 0
-        @param test_size : float
+        test_size : float
             a decimal value between 0 and 1 representing what percent of the dataset to use for testing
-        @param random_state : int
+        random_state : int
             an optional value which selects samples for training/testing from the given seed
-        @param shuffle_choice : bool
+        shuffle_choice : bool
             an optional value which determines whether or not the data is shuffled before splitting
         '''
         self.classifier = svm.SVC(C=C_val, kernel=kernel_choice, gamma=gamma_choice)
         self.data = data
         self.target = target
-        # splitting the testing and training dataset could be done manually, 
-        # just keep in mind the correct format for numpy array inputs, etc.
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.data, self.target,test_size=test_ratio,random_state=random_seed)
-        
         self.y_pred = np.zeros(self.y_test.size)
         pass
 
     def fit(self):
+        '''
+        Trains the classifier on a portion of the dataset
+        '''
         self.classifier.fit(self.x_train, self.y_train)
         pass
 
     def predict(self):
+        '''
+        Tests the classifier on a portion of the dataset
+        '''
         self.y_pred = self.classifier.predict(self.x_test)
         pass
 
     def predictExample(self, example):
         '''
-        @param example : float[][]
+        Passes a sample to the trained classifier for prediction
+
+        Parameters:
+        ---
+        example : numpy.ndarray.dtype(float64)
             contains an array of samples (which are arrays of floats matching the feature dimensions)
+
+        Returns:
+        ---
+            output : int
+                either 0 or 1, corresponding to the label associated with each number
         '''
-        return self.classifier.predict(example)
+        output = self.classifier.predict(example)
+        return output
 
     def getMetrics(self):
+        '''
+        Displays the metrics of the classifier after self.predict
+        '''
         print("Accuracy:",metrics.accuracy_score(self.y_test, self.y_pred))
         print("Precision:",metrics.precision_score(self.y_test, self.y_pred))
         print("Recall:",metrics.recall_score(self.y_test, self.y_pred))
         pass
     
     def plotAcc(self):
+        '''
+        Plots each true label of each sample in the testing set and what the model predicted for each sample
+        '''
         plt.figure()
         plt.plot(range(0, len(self.y_test)), self.y_test, 'b', label='truth')
         plt.plot(range(0,len(self.y_pred)), self.y_pred,'r',label='SVM')
@@ -79,18 +131,28 @@ def data(abs_path, type=True):
     else:
         return df.to_numpy()
 
-def test():
-    from sklearn import datasets
-    cancer = datasets.load_breast_cancer()
-    test = EEGClassifier(cancer.data, cancer.target, 0.3, 109)
-    test.fit()
-    test.predict()
-    test.getMetrics()
-    test.plotAcc()
-    pass
-
 def resample(signal, orig_freq, desired_freq):
     '''
+    Resamples a given signal (or an array of signals) from its given frequency to a new one
+
+    Parameters:
+    ---
+    signal : numpy.ndarray.dtype(float64)
+        Contains a 1d sequence of time frequency data, or a collection of such sequences
+    orig_freq : int
+        Defines the original sampling rate of the given signal
+    desired_freq : int
+        Defines the sampling rate the signal is resampled to
+
+    Returns:
+    ---
+    resampled_signal : numpy.ndarray.dtype(float64)
+        Contains a 1d sequence of the resampled time frequency data, 
+        or a collection of each resampled sequence if there are multiple
+
+    Notes:
+    ---
+    Uses linear interpolation to determine the resampled values
     For multidimensional arrays, only works if all columns share the same length
     '''
     scale = desired_freq/orig_freq
@@ -107,7 +169,6 @@ def resample(signal, orig_freq, desired_freq):
         np.linspace(0.0, 1.0, len(signal), endpoint=False),
         signal)
     return resampled_signal
-
 
 
 if __name__ == '__main__':
@@ -136,9 +197,12 @@ if __name__ == '__main__':
 
     transposed = upsampled.T
 
-    sample = transposed[0][:12]
+    sample = transposed[0]
 
-    print(f"stdev : {statistics.stdev(sample)}, {np.std(sample)}")
-    print(f"mean : {statistics.mean(sample)}, {np.mean(sample)}")
+    # which data should be mapped? The resampled or the original?
+    # print(np.std(sample[:25]))
+    # print(np.std(eye_samps[:12, 0]))
+
+
 
     pass
