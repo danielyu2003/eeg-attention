0a9b293b32af3ecb0ed39abda45262ef463679e6
Daniel Yu <yudaniel136@gmail.com>
2022-07-29 00:48:50 -0400

    update

--- svm_classifier.py
+++ svm_classifier.py
@@ -261,47 +261,106 @@ def interpol(signal, desired_len):
 if __name__ == '__main__':
 
     # No feedback
-
+    print('---no feedback---')
     block1 = pd.read_csv(r"data\eeg\Training_1.csv", header=None)
     bl_1_samples1 = block1.loc[36526:45328]
     bl_1_samples2 = block1.loc[83421:92819]
     bl_1_samples3 = block1.loc[113067:119148]
     bl_1_samples4 = block1.loc[127966:138086]
     bl_1_samples5 = block1.loc[147872:158022]
+    bl_1_samples6 = block1.loc[166446:178348]
+    bl_1_samples7 = block1.loc[118284:200735]
+    bl_1_samples8 = block1.loc[209706:221794]
+    bl_1_samples9 = block1.loc[231059:245642]
+    bl_1_samples10 = block1.loc[253869:265130]
     bl_1_targets1 = getTargets(r"data\eye\BLOCK_1\TRAINING\Trial_1.csv", 51, 256, 844,2610)
     bl_1_targets2 = getTargets(r"data\eye\BLOCK_1\TRAINING\Trial_2.csv", 51, 256, 673,2292)
     bl_1_targets3 = getTargets(r"data\eye\BLOCK_1\TRAINING\Trial_3.csv", 51, 256, 943,2574)
     bl_1_targets4 = getTargets(r"data\eye\BLOCK_1\TRAINING\Trial_4.csv", 51, 256, 733,2598)
     bl_1_targets5 = getTargets(r"data\eye\BLOCK_1\TRAINING\Trial_5.csv", 51, 256, 637,2174)
-    bl_1_samps = pd.concat([bl_1_samples1, bl_1_samples2, bl_1_samples3, bl_1_samples4, bl_1_samples5]).to_numpy()
-    bl_1_targs = np.rint(interpol(np.concatenate((bl_1_targets1, bl_1_targets2, bl_1_targets2, bl_1_targets4, bl_1_targets5)), len(bl_1_samps)))
+    bl_1_targets6 = getTargets(r"data\eye\BLOCK_1\TRAINING\Trial_5.csv", 51, 256, 629,2446)
+    bl_1_targets7 = getTargets(r"data\eye\BLOCK_1\TRAINING\Trial_5.csv", 51, 256, 810,2466)
+    bl_1_targets8 = getTargets(r"data\eye\BLOCK_1\TRAINING\Trial_5.csv", 51, 256, 811,2439)
+    bl_1_targets9 = getTargets(r"data\eye\BLOCK_1\TRAINING\Trial_5.csv", 51, 256, 885,2387)
+    bl_1_targets10 = getTargets(r"data\eye\BLOCK_1\TRAINING\Trial_5.csv", 51, 256, 790,2553)
+    bl_1_samps = pd.concat([bl_1_samples1, bl_1_samples2, bl_1_samples3, bl_1_samples4, bl_1_samples5, bl_1_samples6, bl_1_samples7, bl_1_samples8, bl_1_samples9, bl_1_samples10]).to_numpy()
+    bl_1_targs = np.rint(interpol(np.concatenate((bl_1_targets1, bl_1_targets2, bl_1_targets3, bl_1_targets4, bl_1_targets5, bl_1_targets6, bl_1_targets7, bl_1_targets8, bl_1_targets9, bl_1_targets10)), len(bl_1_samps)))
 
+    # single channel (visual only)
+    print('---visual only---')
+    block2 = pd.read_csv(r"data\eeg\Training_2.csv", header=None)
+    bl_2_samples1 = block2.loc[10323:19655]
+    bl_2_samples2 = block2.loc[34474:45938]
+    bl_2_samples3 = block2.loc[58226:68135]
+    bl_2_samples4 = block2.loc[78378:88097]
+    bl_2_samples5 = block2.loc[96676:106298]
+    bl_2_samples6 = block2.loc[117522:126802]
+    bl_2_samples7 = block2.loc[139286:149302]
+    bl_2_samples8 = block2.loc[167999:177583]
+    bl_2_samples9 = block2.loc[186732:195733]
+    bl_2_samples10 = block2.loc[204428:213935]
+    bl_2_targets1 = getTargets(r"data\eye\BLOCK_2\TRAINING\Trial_1.csv", 51, 256, 1392,3114)
+    bl_2_targets2 = getTargets(r"data\eye\BLOCK_2\TRAINING\Trial_2.csv", 51, 256, 881,3048)
+    bl_2_targets3 = getTargets(r"data\eye\BLOCK_2\TRAINING\Trial_3.csv", 51, 256, 902,2682)
+    bl_2_targets4 = getTargets(r"data\eye\BLOCK_2\TRAINING\Trial_4.csv", 51, 256, 881,2701)
+    bl_2_targets5 = getTargets(r"data\eye\BLOCK_2\TRAINING\Trial_5.csv", 51, 256, 886,2544)
+    bl_2_targets6 = getTargets(r"data\eye\BLOCK_2\TRAINING\Trial_5.csv", 51, 256, 821,2506)
+    bl_2_targets7 = getTargets(r"data\eye\BLOCK_2\TRAINING\Trial_5.csv", 51, 256, 983,2773)
+    bl_2_targets8 = getTargets(r"data\eye\BLOCK_2\TRAINING\Trial_5.csv", 51, 256, 1525,3239)
+    bl_2_targets9 = getTargets(r"data\eye\BLOCK_2\TRAINING\Trial_5.csv", 51, 256, 763,2370)
+    bl_2_targets10 = getTargets(r"data\eye\BLOCK_2\TRAINING\Trial_5.csv", 51, 256, 857,2459)
+    bl_2_samps = pd.concat([bl_2_samples1, bl_2_samples2, bl_2_samples3, bl_2_samples4, bl_2_samples5, bl_2_samples6, bl_2_samples7, bl_2_samples8, bl_2_samples9, bl_2_samples10]).to_numpy()
+    bl_2_targs = np.rint(interpol(np.concatenate((bl_2_targets1, bl_2_targets2, bl_2_targets3, bl_2_targets4, bl_2_targets5, bl_2_targets6, bl_2_targets7, bl_2_targets8, bl_2_targets9, bl_2_targets10)), len(bl_2_samps)))
+    
+    # multimodal (visual and haptic)
+    print('---multimodal---')
     block3 = pd.read_csv(r"data\eeg\Training_3.csv", header=None)
     bl_3_samples1 = block3.loc[16557:26620]
     bl_3_samples2 = block3.loc[37056:46653]
     bl_3_samples3 = block3.loc[68025:78037]
     bl_3_samples4 = block3.loc[87211:97344]
     bl_3_samples5 = block3.loc[117232:126776]
+    bl_3_samples6 = block3.loc[135820:146335]
+    bl_3_samples7 = block3.loc[155019:164164]
+    bl_3_samples8 = block3.loc[186754:198025]
+    bl_3_samples9 = block3.loc[208543:219066]
+    bl_3_samples10 = block3.loc[227654:236925]
     bl_3_targets1 = getTargets(r"data\eye\BLOCK_3\TRAINING\Trial_1.csv", 51, 256, 1088, 2873)
     bl_3_targets2 = getTargets(r"data\eye\BLOCK_3\TRAINING\Trial_2.csv", 51, 256, 789, 2436)
     bl_3_targets3 = getTargets(r"data\eye\BLOCK_3\TRAINING\Trial_3.csv", 51, 256, 1793, 3573)
     bl_3_targets4 = getTargets(r"data\eye\BLOCK_3\TRAINING\Trial_4.csv", 51, 256, 1005, 2804)
     bl_3_targets5 = getTargets(r"data\eye\BLOCK_3\TRAINING\Trial_5.csv", 51, 256, 1836, 3564)
-    bl_3_samps = pd.concat([bl_3_samples1, bl_3_samples2, bl_3_samples3, bl_3_samples4, bl_3_samples5]).to_numpy()
-    bl_3_targs = np.rint(interpol(np.concatenate((bl_3_targets1, bl_3_targets2, bl_3_targets3, bl_3_targets4, bl_3_targets5)), len(bl_3_samps)))
+    bl_3_targets6 = getTargets(r"data\eye\BLOCK_3\TRAINING\Trial_5.csv", 51, 256, 754, 2629)
+    bl_3_targets7 = getTargets(r"data\eye\BLOCK_3\TRAINING\Trial_5.csv", 51, 256, 756, 2557)
+    bl_3_targets8 = getTargets(r"data\eye\BLOCK_3\TRAINING\Trial_5.csv", 51, 256, 1011, 3038)
+    bl_3_targets9 = getTargets(r"data\eye\BLOCK_3\TRAINING\Trial_5.csv", 51, 256, 1071, 2894)
+    bl_3_targets10 = getTargets(r"data\eye\BLOCK_3\TRAINING\Trial_5.csv", 51, 256, 787, 2408)
+    bl_3_samps = pd.concat([bl_3_samples1, bl_3_samples2, bl_3_samples3, bl_3_samples4, bl_3_samples5, bl_3_samples6, bl_3_samples7, bl_3_samples8, bl_3_samples9, bl_3_samples10]).to_numpy()
+    bl_3_targs = np.rint(interpol(np.concatenate((bl_3_targets1, bl_3_targets2, bl_3_targets3, bl_3_targets4, bl_3_targets5, bl_3_targets6, bl_3_targets7, bl_3_targets8, bl_3_targets9, bl_3_targets10)), len(bl_3_samps)))
 
     multimodal = EEGClassifier(bl_3_samps, bl_3_targs)
+    print("fitting multimodal")
     multimodal.fit()
+
+    visual = EEGClassifier(bl_2_samps, bl_2_targs)
+    print("fitting visual")
+    visual.fit()
     
     no_feed = EEGClassifier(bl_1_samps, bl_1_targs)
+    print("fitting no_feed")
     no_feed.fit()
-    no_feed.predict()
 
-    print("multimodal performance: \n")
+    print("multimodal performance:")
     multimodal.predictExample(bl_1_samps, bl_1_targs, metric=True)
-    print("\n")
-    print("no feedback perfomance: \n")
-    no_feed.getMetrics()
-
+    print('---')
+    multimodal.predictExample(bl_2_samps, bl_2_targs, metric=True)
+    print("visual perfomance:")
+    visual.predictExample(bl_1_samps, bl_1_targs, metric=True)
+    print('---')
+    visual.predictExample(bl_3_samps, bl_3_targs, metric=True)
+    print("no_feed perfomance:")
+    no_feed.predictExample(bl_2_samps, bl_2_targs, metric=True)
+    print('---')
+    no_feed.predictExample(bl_3_samps, bl_3_targs, metric=True)
 
     pass
